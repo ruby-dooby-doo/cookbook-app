3b4d02bf8631286689821f8b256c76f7662d9317
Brian <riceb53@gmail.com>
2018-06-26 18:33:58 -0500

    namespace in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-  get "/one_recipe_url" => "api/recipes#one_recipe_action"
+  namespace :api do
+    get "/one_recipe_url" => "recipes#one_recipe_action"
+  end
 end
