af26a30e9235d5400b2d0ebcdfefa3a7283c2504
Brian <riceb53@gmail.com>
2018-06-28 19:30:28 -0500

    make show action for recipes

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -3,4 +3,13 @@ class Api::RecipesController < ApplicationController
     @recipes = Recipe.all
     render "index.json.jbuilder"
   end
+
+  def show
+    # grab information from params to determine which recipe to show to the user
+    recipe_id = params[:id]
+    # grab a particular recipe from the db
+    @recipe1 = Recipe.find_by(id: recipe_id)
+    # show that recipe to the user
+    render "show.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
     # index. shows all the recipes
-    get "/recipes" => "recipes#index"
+    get '/recipes' => 'recipes#index'
+    # show. displays one particular recipe
+    get '/recipes/:id' => 'recipes#show'
   end
 end
