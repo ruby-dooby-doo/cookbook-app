04dcd13c4ae6e969fce386bebf92210d789e05db
Brian <riceb53@gmail.com>
2018-08-08 20:30:08 -0500

    replace vanilla js with vuejs

--- public/index.js
+++ public/index.js
@@ -1,47 +1,32 @@
-/* global axios */
+/* global Vue, VueRouter, axios */
 
-console.log('in index.js');
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      recipes: []
+    };
+  },
+  created: function() {
+    console.log('in created function')
+    axios.get('/api/recipes').then(function(response) {
+      console.log(response.data);
+      this.recipes = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
 
-// get the box
-// get the thing you want to put in the box
-// copy the thing and put it in the box
-
-var recipeTemplate = document.querySelector('#recipe-card');
-
-var recipeContainer = document.querySelector('.row');
-
-
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-
-// get data from api
-axios.get("/api/recipes").then(function(response) {
-  // this is the data from the api
-  var recipes = response.data;
-
-  // loop through all the data from the api
-  recipes.forEach(function(recipe) {
-    // for each recipe in the db...
-    console.log(recipe);
-    // copy template
-    var recipeClone = recipeTemplate.content.cloneNode(true);
-    // modify template
-    recipeClone.querySelector('.card-title').innerText = recipe.title;
-    recipeClone.querySelector('.card-text').innerText = recipe.directions;
-    recipeClone.querySelector('.id').innerText = "id: " + recipe.id;
-    recipeClone.querySelector('.user').innerText = "user name: " + recipe.user.name;
-    recipeClone.querySelector('.prep ').innerText = "prep time: " + recipe.prep_time;
-
-    // add to DOM
-    recipeContainer.appendChild(recipeClone);
-  });
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage }],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
 });
 
-// response = Unirest.get("/api/recipes")
-// p response.body
-
-// axios.get("/api/recipes").then(function(response) {
-//   console.log('i am on line 43');
-//   console.log(response.data);
-// });
-
-// console.log('i am after line 43, on line 47');
+var app = new Vue({
+  el: "#vue-app",
+  router: router
+});
