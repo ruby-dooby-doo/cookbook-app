63bf5f3dfa20209d5c72b4e902530449502ef3c0
Brian <riceb53@gmail.com>
2018-08-08 21:00:17 -0500

    make random route

--- public/index.js
+++ public/index.js
@@ -1,5 +1,17 @@
 /* global Vue, VueRouter, axios */
 
+var RandomPage = {
+  template: "#random-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js! from the random page hahahah"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
 var HomePage = {
   template: "#home-page",
   data: function() {
@@ -20,7 +32,10 @@ var HomePage = {
 };
 
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/random", component: RandomPage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
